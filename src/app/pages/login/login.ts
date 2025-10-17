fa4486939f7e3c449e7fcc163260aa8112fe80b6
Natig07 <elizade.natiq58@gmail.com>
2025-10-17 14:34:02 +0400

    Initial commit - upload Angular project

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  showpassword=false;
+
+  togglePasswordVisibility() {
+    this.showpassword = !this.showpassword;
+  }
+
+}
